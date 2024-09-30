fbed3f999a994fc73f3273002cd53e46265d5763
longfangsong <longfangsong@icloud.com>
2024-09-30 19:14:30 +0200

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  DarkThemeToggle,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+  ThemeModeScript,
+} from "flowbite-react";
 
 const inter = Inter({ subsets: ["latin", "latin-ext"] });
 
@@ -9,6 +18,33 @@ export const metadata: Metadata = {
   description: "An app for learning Swedish",
 };
 
+async function NavBar() {
+  return (
+    <Navbar
+      fluid
+      className="items-center justify-between border-b border-gray-200 bg-white text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400"
+    >
+      <NavbarBrand href={process.env.CF_PAGES_URL}>
+        <span className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold text-gray-900 dark:text-white">
+          Lära Svenska
+        </span>
+      </NavbarBrand>
+      <div className="flex flex-row justify-between items-center gap-2 md:order-2">
+        <DarkThemeToggle />
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href={process.env.CF_PAGES_URL + "/articles"}>
+          Articles
+        </NavbarLink>
+        <NavbarLink href={process.env.CF_PAGES_URL + "/words"}>
+          Words
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,7 +52,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ThemeModeScript />
+      </head>
+      <body className={inter.className}>
+        <NavBar />
+        <main className="p-2 sm:p-4">{children}</main>
+      </body>
     </html>
   );
 }
