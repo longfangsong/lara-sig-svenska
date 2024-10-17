55dfde34effad854c8ab689035746525dba4e270
longfangsong <longfangsong@icloud.com>
2024-10-17 11:43:44 +0200

    UI test for query words in articles

--- ui_tests/articles.spec.ts
+++ ui_tests/articles.spec.ts
@@ -15,7 +15,28 @@ test("can see articles", async ({ page }) => {
   );
   const sentences = page.locator("main > div > div");
   expect(await sentences.count()).toBeGreaterThan(2);
-  expect(sentences.first()).toContainText(
+  const firstSentence = sentences.first();
+  expect(firstSentence).toContainText(
     "Landshövdingen i Stockholm Anna Kinberg Batra har fått sparken från sitt jobb.",
   );
+  const sittElement = firstSentence.locator("a").nth(10);
+  expect(sittElement).toHaveText("sitt");
+  await sittElement.click();
+});
+
+test("can query words", async ({ page }) => {
+  await page.goto("/articles/165eaf64-6e5a-424d-839f-b2fbf6e0ea0a");
+  const sentences = page.locator("main > div > div");
+  const firstSentence = sentences.first();
+  expect(firstSentence).toContainText(
+    "Landshövdingen i Stockholm Anna Kinberg Batra har fått sparken från sitt jobb.",
+  );
+  const sittElement = firstSentence.locator("a").nth(10);
+  expect(sittElement).toHaveText("sitt");
+  await sittElement.click();
+  await page.waitForTimeout(1000);
+  const dialog = page.locator('[role="dialog"]');
+  await expect(dialog).toBeVisible();
+  const firstLemma = dialog.locator("h2").first();
+  await expect(firstLemma).toContainText("sin");
 });
